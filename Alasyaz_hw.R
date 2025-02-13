574190f390605a5c78544b35e909378c479fb175
Alasya Zeweldi <az5922a@american.edu>
2025-02-12 22:03:53 -0500

    Computed the mean of 100 random normal numbers

--- Alasyaz_hw.R
+++ Alasyaz_hw.R
@@ -1,2 +1,4 @@
 random_numbers <- rnorm(100, mean = 25, sd = 1)
 saveRDS(random_numbers, file = "random_numbers.rds")
+mean_value <- mean(random_numbers)
+saveRDS(mean_value, file = "mean_value.rds")
